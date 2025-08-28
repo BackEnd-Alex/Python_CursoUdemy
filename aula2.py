5f316a50f389465ed649498501644a918e9bc90f
Alexsandro <contadetestealexsandro@gmail.com>
2025-08-28 14:33:45 -0300

    Aprendendo Python, com Udemy

---
+++ aula2.py
@@ -0,0 +1,4 @@
+print(12, 34, sep=' - ', end='\r\n')
+print(56, 78)
+print("Alexsandro \"Mendes")
+print(type('alex'))
---
+++ exercicio_Secao3.py
@@ -0,0 +1,13 @@
+name = 'Alexsandro'
+lastName = 'Mendes Brndão'
+age = 42
+birthYear = 2025 - 42
+isAdult = age >= 18
+heigth = 1.70
+
+print('Nome:', name)
+print('Sobrenome:', lastName)
+print('Idade:', age)
+print('Ano de Nascimento:', birthYear)
+print('É maior de idade?:', isAdult)
+print('Alturas em metros:', heigth)
